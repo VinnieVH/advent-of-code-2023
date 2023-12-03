a6291843e381e54b206cd590675ac5259085a04e
Vincent Van Herreweghe <vincent@vincent.local>
2023-12-03 23:10:28 +0100

    finish day 1 challenge 1

--- day1/challenge-1.ts
+++ day1/challenge-1.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import { getInputValues } from "../utils";
+
+const values = getInputValues("./day1/input.txt");
+let total = 0;
+
+values.forEach(value => {
+    const calibrationValue = value.replace(/[^0-9]/g, '');
+    const firstDigit = calibrationValue.slice(0, 1);
+    const lastDigit = calibrationValue.slice(-1);
+
+    total += +`${firstDigit}${lastDigit}`;
+});
+
+console.log(total);
